4b62523a6b996ce3083cfdf681d61694d27ed858
iespindola <iespindola@educativa.com>
2022-02-08 20:30:46 -0300

    Demo

--- plots/demo.py
+++ plots/demo.py
@@ -4,11 +4,17 @@ from matplotlib.patches import Ellipse
 
 NUM = 250
 
-fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
+ells = [Ellipse(xy=np.random.rand(2) * 10,
+                width=np.random.rand(), height=np.random.rand(),
+                angle=np.random.rand() * 360)
+        for i in range(NUM)]
 
-x = np.array([[p0x], [pfx]])
-y = np.array([[p0y], [pfy]])
-plt.plot(x, y)
+fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
+for e in ells:
+    ax.add_artist(e)
+    e.set_clip_box(ax.bbox)
+    e.set_alpha(np.random.rand())
+    e.set_facecolor(np.random.rand(3))
 
 ax.set_xlim(0, 10)
 ax.set_ylim(0, 10)
